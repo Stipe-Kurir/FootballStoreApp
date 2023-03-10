495378ff22e2dc6f9234da7e188941e330e183e6
Stipe-Kurir <skurir00@fesb.hr>
2023-03-10 20:32:47 +0100

    Footer design

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -3,8 +3,48 @@ import "./footer.css"
 
 const Footer = () => {
   return (
-    <div>
-      FOOTER
+    <div className="footerContainer">
+      <div className="footerElements">
+
+        <div className="footerTitle">POSLOVNICE</div>
+
+        <div className="footerDesc">
+          <div className="footerDescOne">
+            <div className="footerDescOneTitle">ZAGREB</div>
+            <div className="footerDescOneInfo">Zagrebačka 7<br/>PON/SUB 9-21 h / NED 9-14 h</div>
+          </div>
+          <div className="footerDescTwo">
+            <div className="footerDescTwoTitle">SPLIT</div>
+            <div className="footerDescTwoInfo">Splitska 7<br/>PON/NED 9-21 h</div></div>
+        </div>
+
+        <div className="footerSlider">
+          <div className="footerItems">
+
+            <div className="footerItem">
+             <img className="footerImg" src="./images/store1.jpg" alt=" "  ></img>
+            </div>
+            <div className="footerItem">
+             <img className="footerImg" src="./images/store2.jpg" alt=" "  ></img>
+            </div>
+            <div className="footerItem">
+             <img className="footerImg" src="./images/store3.jpg" alt=" "  ></img>
+            </div>
+            <div className="footerItem">
+              <img className="footerImg" src="./images/store4.jpg" alt=" " ></img>
+            </div>
+            <div className="footerItem">
+             <img className="footerImg" src="./images/store3.jpg" alt=" "  ></img>
+            </div>
+            <div className="footerItem">
+              <img className="footerImg" src="./images/store4.jpg" alt=" " ></img>
+            </div>
+  
+          </div>
+        </div>
+
+        <div className="footerInfo">INFO u stupcima</div>
+      </div>
     </div>
   )
 }
